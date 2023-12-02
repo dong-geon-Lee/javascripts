46518d9b5adb8709fad408ec465e7102f5879bb9
developer <ch9716@naver.com>
2023-12-02 18:07:57 +0900

    자바스크립트 제이쿼리 연습

--- javasrcipt/script.js
+++ javasrcipt/script.js
@@ -1,1 +1,48 @@
-alert("경고창");
+function appInit() {
+  $(document).ready(() => {
+    initializeUI();
+    setupEventHandlers();
+    showAndHideBtn();
+    fetchDatas();
+    handleFormData();
+  });
+}
+
+const initializeUI = () => {
+  $("#content h1").text("JQuery로 변경된 텍스트!");
+};
+
+const setupEventHandlers = () => {
+  $("#myButton").click(() => alert("버튼이 클릭되었습니다"));
+  $("#toggleButton").click(() => $("#myDiv").toggle());
+};
+
+const showAndHideBtn = () => {
+  $("#hideButton").click(() => $("#someEl").hide());
+  $("#showButton").click(() => $("#someEl").show());
+  $("#toggleButtons").click(() => $("#someEl").toggle());
+};
+
+const fetchDatas = () => {
+  $.ajax({
+    url: "https://jsonplaceholder.typicode.com/todos",
+    type: "GET",
+    success: (res) => {
+      console.log(res);
+    },
+    error: (err) => {
+      console.log(err);
+    },
+  });
+};
+
+const handleFormData = () => {
+  $(".myform").submit((e) => {
+    e.preventDefault();
+    console.log("제출 완료");
+  });
+};
+
+const handleColor = () => {};
+
+appInit();
